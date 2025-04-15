d34b132bdfbdf7e933e4fd033055fbbba4709434
SutuSebastian <sebiitv@gmail.com>
2025-04-15 09:51:27 +0300

    persist theme mode; fix "missing React" error

--- client/main.tsx
+++ client/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { initThemeMode } from "flowbite-react";
 import { Meteor } from "meteor/meteor";
+import { createRoot } from "react-dom/client";
 import { App } from "/imports/ui/App";
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container!);
   root.render(<App />);
+  initThemeMode();
 });
