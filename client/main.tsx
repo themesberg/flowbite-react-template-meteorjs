1d410893646ae0cc5c9f45493ed45beefd41b82e
Sutu Sebastian <sebiitv@gmail.com>
2025-07-22 15:43:47 +0300

    up packages

--- client/main.tsx
+++ client/main.tsx
@@ -3,10 +3,17 @@ import { initThemeMode } from "flowbite-react";
 import { Meteor } from "meteor/meteor";
 import { createRoot } from "react-dom/client";
 import { App } from "/imports/ui/App";
+// import { ThemeInit } from "../.flowbite-react/init";
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container!);
-  root.render(<App />);
+  root.render(
+    <>
+      {/* TODO: Uncaught Error: Cannot find module '../.flowbite-react/init' */}
+      {/* <ThemeInit /> */}
+      <App />
+    </>,
+  );
   initThemeMode();
 });
